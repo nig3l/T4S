e09c12df48833e0fc1cf016638a86aeb7b22e289
nigel <nigelchimwene@gmail.com>
2024-05-27 09:56:04 +0300

    components

--- Intro/React/talent_4_startups/src/App.js
+++ Intro/React/talent_4_startups/src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+}from "react-router-dom";
+import Home from "./components/home";
+import About from "./components/about";
+import Contact from "./components/contact";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+   
+        <div className="App">
+            <ul className="App-header">
+                <li>
+                    <Link to="/"Home></Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        About Us
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/contact">
+                        Contact Us
+                    </Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home />}
+                ></Route>
+                <Route
+                    path="/about"
+                    element={<About />}
+                ></Route>
+                <Route
+                    path="/contact"
+                    element={<Contact />}
+                ></Route>
+            </Routes>
+        </div>
+    </Router>
   );
 }
 
