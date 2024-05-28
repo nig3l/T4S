7bbb00f62e86433cfba1c36e1a7cf35b47674e42
nigel <nigelchimwene@gmail.com>
2024-05-28 15:28:38 +0300

    routers update

--- Intro/React/talent_4_startups/src/App.js
+++ Intro/React/talent_4_startups/src/App.js
@@ -1,52 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-}from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
 import Contact from "./components/contact";
 
-
 function App() {
   return (
     <Router>
-   
-        <div className="App">
-            <ul className="App-header">
-                <li>
-                    <Link to="/"Home></Link>
-                </li>
-                <li>
-                    <Link to="/about">
-                        About Us
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/contact">
-                        Contact Us
-                    </Link>
-                </li>
-            </ul>
-            <Routes>
-                <Route
-                    path="/"
-                    element={<Home />}
-                ></Route>
-                <Route
-                    path="/about"
-                    element={<About />}
-                ></Route>
-                <Route
-                    path="/contact"
-                    element={<Contact />}
-                ></Route>
-            </Routes>
-        </div>
+      <div className="App">
+        <ul className="App-header">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+      </div>
     </Router>
   );
 }
